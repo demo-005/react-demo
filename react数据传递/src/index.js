5b6568b2e049c924df7b29c34fbedcef35714896
wb-twh369668 <wb-twh369668@alibaba-inc.com>
2020-07-22 19:14:12 +0800

    删除无用的代码和文件夹

--- react数据传递/src/index.js
+++ react数据传递/src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginControl from "./conpontents/LoginControl"
-import {HashRouter,Route,Switch } from "react-router-dom"
-import Register from './containers/register/Register';
-import Login from './containers/login/Login';
-import Main from './containers/main/Main';
 
 ReactDOM.render(
   <LoginControl/>,
